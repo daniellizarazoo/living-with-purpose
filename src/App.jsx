103ac24cc45cf0705315c1ac5e0b499848dba7ad
daniellizarazoo <daniellizarazo015@gmail.com>
2024-12-28 16:44:02 +0100

    Added analytics to vercel page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import LandingPage from './pages/LandingPage'
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
   return (
     <>
       <LandingPage/>
+      <Analytics />
     </>
   )
 }
